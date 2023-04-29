3282a070e0cf570de1a12eca87d00c52885bcb0d
Luke Shumaker <lukeshu@lukeshu.com>
2023-04-28 21:32:27 -0600

    tidy: Centralize the ktlint configuration

--- build-logic/plugins/src/main/java/ktlint.gradle.kts
+++ build-logic/plugins/src/main/java/ktlint.gradle.kts
@@ -1,7 +1,21 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+
+val libs = the<LibrariesForLibs>()
+
 plugins {
   id("org.jlleitschuh.gradle.ktlint")
 }
 
 ktlint {
   version.set("0.49.1")
+
+  filter {
+    exclude { entry ->
+      entry.file.toString().contains("build/generated/source/wire")
+    }
+  }
+}
+
+dependencies {
+  ktlintRuleset(libs.ktlint.twitter.compose)
 }
